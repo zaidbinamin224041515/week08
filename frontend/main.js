062a1ab1582eea5c2d747e2b4ca2750bcadf754f
zaid0411 <zaidbinamin04@gmail.com>
2025-09-12 14:46:19 +1000

    Point frontend to live Product/Order IPs (no /api)

--- frontend/main.js
+++ frontend/main.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // API endpoints for the Product and Order services.
     // These ports (30000 for Product, 30001 for Order) are mapped
     // from the Docker containers to the host machine in docker-compose.yml for Example 2.
-    const PRODUCT_API_BASE_URL = '_PRODUCT_API_URL_';
-    const ORDER_API_BASE_URL = '_ORDER_API_URL_';
+    const PRODUCT_API_BASE_URL = 'http://4.195.19.6:8000';
+    const ORDER_API_BASE_URL = 'http://4.237.176.120:8001';
 
     // Product Service is named 'product-service-w04e2' and exposes port 8000 internally.
-    //const PRODUCT_API_BASE_URL = 'http://product-service-w04e2:8000';
+    //const PRODUCT_API_BASE_URL = 'http://4.195.19.6:8000';
     // Order Service is named 'order-service-w04e2' and exposes port 8001 internally.
-    //const ORDER_API_BASE_URL = 'http://order-service-w04e2:8001';
+    //const ORDER_API_BASE_URL = 'http://4.237.176.120:8001';
 
     // DOM Elements
     const messageBox = document.getElementById('message-box');
